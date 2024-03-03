19a78c0f597192207c14b3c88b3e5f577aa62439
unknown <Christagnissan75@gmail.com>
2024-03-03 14:08:10 +0000

    second commit

--- my-app/src/components/poubelle.js
+++ my-app/src/components/poubelle.js
@@ -2,14 +2,12 @@ import React, { useEffect } from 'react';
 import '../css/poubelle.css'; // Assuming your styles are in App.css
 import { database } from '../firebase';
 import { onValue, ref, set } from "firebase/database";
+import { ToastContainer, toast } from "react-toastify";
 
-
-/*console.log(chemin);
-console.log(database);*/
 
 const Poubelle = () => {
   const [etat, setEtat] = React.useState({});
-    const iconStyles = {
+  const iconStyles = {
     fontSize: '150px',
     position:'absolute',
     left: '50%',
@@ -23,14 +21,25 @@ const Poubelle = () => {
       const data = snapshot.val();
       setEtat(data);
     });
-  }, [chemin]);
-  
-  
-
+  }, []);
+  const notify = (message, type = "warn") => {
+    toast[type](message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  };
+ 
   const fermer = () => {
     // Implement closing logic here (e.g., toggle visibility of hidden content)
     //setEtat({ ...etat, verrouillage: !etat.verrouillage }); // Toggle lock state
     set(chemin,{ ...etat, verrouillage: !etat.verrouillage })
+    notify("BONJOUR")
   };
 
    /*const notify = () =>{
@@ -39,33 +48,39 @@ const Poubelle = () => {
 
    
    
-  return (
-    <div className="poubelle-container">
-      <ion-icon name="trash-outline" className="trash-icon" style={iconStyles} />
-      <div className="container">
-       
-        <button className="fermer" onClick={fermer}>
-          {etat.verrouillage ? 'Déverrouiller' : 'Vérrouiller'}
-        </button>
+  return false ? ((
+    <><div className="poubelle-container">
+    <ion-icon name="trash-outline" className="trash-icon" style={iconStyles} />
+    <div className="container">
+     
+      <button className="fermer" onClick={fermer}>
+        {etat.verrouillage ? 'Déverrouiller' : 'Vérrouiller'}
+      </button>
+    </div>
+    <div className="informations">
+      <div className="remplissage info">
+        <ion-icon name="trash-outline" />
+        <h4>Niveau de remplissage: {etat.remplissage}%</h4>
+        
       </div>
-      <div className="informations">
-        <div className="remplissage info">
-          <ion-icon name="trash-outline" />
-          <h4>Niveau de remplissage: {etat.remplissage}%</h4>
-          
-        </div>
-        <div className="humidite info">
-          <ion-icon name="speedometer-sharp" />
-          <h4>Taux d'humidité: {etat.humidite}%</h4>
-          {/* Update data here to reflect actual humidity */}
-        </div>
-        <div className={etat.verrouillage  ? 'info verrouillage' : 'info active'}>
-          <ion-icon name={etat.verrouillage ? 'lock-closed-sharp' : 'lock-open-sharp'} />
-          <h4>{etat.verrouillage  ? 'Verrouillée' : 'Non vérouillée'}</h4>
-        </div>
+      <div className="humidite info">
+        <ion-icon name="speedometer-sharp" />
+        <h4>Taux d'humidité: {etat.humidite}%</h4>
+        {/* Update data here to reflect actual humidity */}
+      </div>
+      <div className={etat.verrouillage  ? 'info verrouillage' : 'info active'}>
+        <ion-icon name={etat.verrouillage ? 'lock-closed-sharp' : 'lock-open-sharp'} />
+        <h4>{etat.verrouillage  ? 'Verrouillée' : 'Non vérouillée'}</h4>
       </div>
     </div>
-  );
-};
-
+    
+  </div>
+  
+  </>
+    
+  )) : (<>
+  <>BONJOUR</>
+  <ToastContainer/>
+  </>);
+}
 export default Poubelle;
